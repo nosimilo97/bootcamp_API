402499cb0e5040642131fe68f35b27d730894bfa
nosimilo97 <celiwe097@gmail.com>
2024-08-21 21:18:10 +0200

    added changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-
+// const cors = require('cors');
 app.use(express.urlencoded({extended: false }))
 app.use(express.json())
 app.use(express.static('public'));
